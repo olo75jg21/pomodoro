e8a34b31ca045c21851cf8c00005e765b128e2fe
Kuba <olo75jg12@gmail.com>
2022-06-17 01:32:48 +0200

    Updates

---
+++ precache-manifest.30139bf9c0a84a817cf972476a4839a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "56047f1c98db7f0fde2b",
+    "url": "/pomodoro/static/css/main.5f328fbf.chunk.css"
+  },
+  {
+    "revision": "56047f1c98db7f0fde2b",
+    "url": "/pomodoro/static/js/main.56047f1c.chunk.js"
+  },
+  {
+    "revision": "b78eb0ab234ee01f104f",
+    "url": "/pomodoro/static/css/1.ebaab271.chunk.css"
+  },
+  {
+    "revision": "b78eb0ab234ee01f104f",
+    "url": "/pomodoro/static/js/1.b78eb0ab.chunk.js"
+  },
+  {
+    "revision": "a702e67ff471c6ecfc33",
+    "url": "/pomodoro/static/js/runtime~main.a702e67f.js"
+  },
+  {
+    "revision": "376fae61295759e775152283dc418c79",
+    "url": "/pomodoro/index.html"
+  }
+];
