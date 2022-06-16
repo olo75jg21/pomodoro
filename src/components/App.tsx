f2f4df39d178b94e67ad48e8084600d6d7e03122
Kuba <olo75jg12@gmail.com>
2022-06-16 21:49:03 +0200

    basic functionalities

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export const App = () => {
+import '../App.css';
+
+import { Pomodoro } from './Pomodoro';
+import { SettingsContext } from './SettingsContext';
+
+import { defaultISettings } from './SettingsContext';
+
+export const App = (): JSX.Element => {
+  const [showSettings, setShowSettings] = useState<boolean>(defaultISettings.showSettings);
+  const [workMinutes, setWorkMinutes] = useState<number>(defaultISettings.workMinutes);
+  const [breakMinutes, setBreakMinutes] = useState<number>(defaultISettings.breakMinutes);
+
   return (
-    <div>Hello</div>
-  )
-}
+    <div className='container center'>
+      <SettingsContext.Provider value={{
+        showSettings,
+        workMinutes,
+        breakMinutes,
+        setShowSettings,
+        setWorkMinutes,
+        setBreakMinutes
+      }}>
+        <Pomodoro />
+      </SettingsContext.Provider>
+    </div>
+  );
+};
